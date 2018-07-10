07f0827f1f3aeb6be5bda8907ca90a618fef279b
Josh Wilson <joshua.wilson@vanderbilt.edu>
2018-07-10 09:45:21 -0500

    Optimized, modularized, and documented the PIDController class.

--- starLib/src/main/java/edu/illinois/mitra/starl/objects/PIDController.java
+++ starLib/src/main/java/edu/illinois/mitra/starl/objects/PIDController.java
@@ -1,7 +1,18 @@
 package edu.illinois.mitra.starl.objects;
 
+import java.util.Arrays;
+
 /**
- * Created by VerivitalLab on 1/22/2016.
+ * A PID (proportional-integral-derivative) controller implementation, with
+ * additional modifications of saturation limit, wind-up limit, and lowpass
+ * filtering of the derivative signal.
+ *
+ * Given a setpoint value and a current value, the PID controller will return command values
+ * that smoothly bring the current value to the setpoint value, depending on the three constants
+ * Kp, Ki, and Kd. See https://en.wikipedia.org/wiki/PID_controller#Loop_tuning for information
+ * on how to find optimal values.
+ *
+ * Created by VerivitalLab on 1/22/2016, updated 7/10/18.
  */
 public class PIDController {
 
@@ -10,20 +21,18 @@ public class PIDController {
     private final double Ki;
     private final double Kd;
 
-    //
+    // see getSaturationLimit(), getWindUpLimit(), getFilterLength()
     private final double saturationLimit;
-
-    //
     private final double windUpLimit;
 
-    //
-    private final double[] filtArray; // the array implementing the filter
-    private int filtIndex;
+    private final double[] filtArray; // the array implementing the filter ring buffer
+    private double filtRunningSum; // the running sum of the elements in filtArray
+    private int filtIndex; // the next index to write to in filtArray
 
-    private int numCommands;
-    private double prevError;
-    private double cumError;
-    private long prevTime;
+    private boolean notFirstCall; // true implies prevError and prevTime are valid
+    private double prevError; // the error recorded on the last call to getCommand()
+    private double cumError; // the accumulated error so far
+    private long prevTime; // the time recorded on the last call to getCommand()
 
 
     /**
@@ -85,79 +94,59 @@ public class PIDController {
         this.filtArray = new double[filterLength >= 1 ? filterLength : 1];
     }
 
-    public double getCommand(double current_val, double set_point) {
-        // find error (P)
-        double error = set_point - current_val;
+    /**
+     * Execute the PID algorithm. Call repeatedly and apply the returned value to the actuator,
+     * in order to physically move the current value closer to the set point.
+     *
+     * @param currentVal the current value of some system property
+     * @param setPoint the desired value for the system property
+     * @return a command value used to bring the current value closer to the set point
+     */
+    public double getCommand(double currentVal, double setPoint) {
+        return getCommand(setPoint - currentVal);
+    }
 
+    /**
+     * Execute the PID algorithm. Call repeatedly and apply the returned value to the actuator,
+     * in order to physically move the current value closer to the set point.
+     *
+     * @param error the difference between the desired value and the current value
+     *              of some system property
+     * @return a command value used to reduce the error
+     */
+    public double getCommand(double error) {
         // find change in error (junk if this is the first call to this method)
         double deltaError = error - prevError;
         prevError = error;
 
-        // find change in time (junk if this is the first call to this method)
-        long currentTime = System.nanoTime();
-        long deltaTime = currentTime - prevTime;
-        prevTime = currentTime;
-        // convert deltaTime to double and seconds
-        double deltaTimeDouble = deltaTime * 1e-9;
+        // find change in time (0 if this is the first call to this method)
+        double deltaTime = getDeltaTime();
 
-        // find accumulated error (I)
-        cumError += error;
-        // limit error if needed and if windUpLimit is not zero or negative
-        if (windUpLimit > 0.0) {
-            if (cumError > windUpLimit) {
-                cumError = windUpLimit;
-            }
-            if (cumError < -windUpLimit) {
-                cumError = -windUpLimit;
-            }
-        }
+        // calculate the command value, adding the P, I, and D components
+        double command = getPComponent(error)
+                       + getIComponent(error, deltaTime)
+                       + getDComponent(deltaError, deltaTime);
 
-        double command = 0;
-
-        // filter change in error, only if not first call to the method (D)
-        if (numCommands > 0) {
-            filtArray[filtIndex] = deltaError;
-            double sum = 0;
-            int currentLength = Math.min(numCommands, filtArray.length);
-            for (int i = 0; i < currentLength; i++) {
-                sum += filtArray[i];
-            }
-            double filtDeltaError = sum / currentLength;
-            // increment filtIndex and reset to zero if too large
-            filtIndex = (filtIndex + 1) % filtArray.length;
-
-            // calculate D part of command value
-            command = Kd * filtDeltaError / deltaTimeDouble;
-        }
-
-        // calculate and add P and I parts of command value
-        command += Kp * error
-                +  Ki * cumError * deltaTimeDouble;
-
-        // limit command value if needed and if saturationLimit is not zero or negative
-        if (saturationLimit > 0.0) {
-            if (command > saturationLimit) {
-                command = saturationLimit;
-            }
-            if (command < -saturationLimit) {
-                command = -saturationLimit;
-            }
-        }
-        // increment recorded number of calls to this method
-        numCommands++;
+        // limit command value if needed
+        command = cap(command, saturationLimit);
+        // lastly, record that this method has been called => prevError and prevTime are valid
+        notFirstCall = true;
 
         return command;
     }
 
-
+    /**
+     * Erase the controller's history, making it function as if it had just been instantiated.
+     */
     public void reset() {
+        Arrays.fill(filtArray, 0.0);
+        filtRunningSum = 0;
+        filtIndex = 0;
+        notFirstCall = false;
         prevError = 0;
         cumError = 0;
-        numCommands = 0;
-        filtIndex = 0;
         prevTime = 0;
     }
-
 
     public double getKp() {
         return Kp;
@@ -191,9 +180,63 @@ public class PIDController {
      * Represents the length of the lowpass filter (moving average) used to
      * compensate for high-frequency changes in the setpoint. Larger values
      * will smooth out the derivative term at the expense of slower response.
-     * The default length is 1 (no smoothing), but larger lengths are recommended.
+     * The default length is 1 (no smoothing), but larger lengths (~8) are recommended.
      */
     public double getFilterLength() {
         return filtArray.length;
+    }
+
+
+    private double getDeltaTime() {
+        // find change in time (0 if this is the first call to this method)
+        long currentTime = System.nanoTime();
+        long deltaTimeLong = currentTime - prevTime;
+        prevTime = currentTime;
+        // convert deltaTime to double and seconds
+        return (notFirstCall ? deltaTimeLong * 1e-9 : 0);
+    }
+
+    private double getPComponent(double error) {
+        // calculate P component of command value
+        return Kp * error;
+    }
+
+    private double getIComponent(double error, double deltaTime) {
+        // find accumulated error
+        cumError += error * deltaTime;
+        // limit error if needed
+        cumError = cap(cumError, windUpLimit);
+        // calculate I component of command value
+        return Ki * cumError;
+    }
+
+    private double getDComponent(double deltaError, double deltaTime) {
+        if (notFirstCall) {
+            // filter change in error using a running sum
+            filtArray[filtIndex] = deltaError;
+            // add newest entry
+            filtRunningSum += filtArray[filtIndex];
+            // increment filtIndex and reset to zero if too large, index of oldest entry or 0
+            filtIndex = (filtIndex + 1) % filtArray.length;
+            // subtract oldest entry or 0
+            filtRunningSum -= filtArray[filtIndex];
+
+            // calculate D component of command value
+            return Kd * filtRunningSum / (filtArray.length * deltaTime);
+        }
+        return 0;
+    }
+
+    private static double cap(double value, double absLimit) {
+        // limits the absolute value of 'value' to positive or negative absLimit, if absLimit is positive
+        if (absLimit > 0) {
+            if (value > absLimit) {
+                return absLimit;
+            }
+            if (value < -absLimit) {
+                return -absLimit;
+            }
+        }
+        return value;
     }
 }
